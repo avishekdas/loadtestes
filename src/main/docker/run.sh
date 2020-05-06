ac24d073c39a39d01762c5988e39d92e435a13b4
Avishek Das <avishekdas.cool@gmail.com>
2020-05-06 22:13:39 +0530

    Update run.sh

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "********************************************************"
-echo "Starting loadtestes Service
+echo "Starting loadtestes Service"
 echo "********************************************************"
 java -Djava.security.egd=file:/dev/./urandom -Dserver.port=$SERVER_PORT   \
      -Dspring.profiles.active=$PROFILE -jar /usr/local/loadtestes/loadtestes-0.0.1-SNAPSHOT.jar
